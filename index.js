fb339703571499f04f29e2720c60e50fca1712fe
Ichbinositu <hesbon.ezekiel@student.moringaschool.com>
2024-01-17 20:38:20 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+// creating event listener using addEventListener using event to listen and a callback function to "handle" the event
+
 function addingEventListener() {
+    const input = document.getElementById('button');
+    input.addEventListener('click', function() {
+    alert('I was clicked!');
+});
 }
+
+// calling outer function
+addingEventListener();
+
+
